d0606da54c6f6592f87f4d53a2a2bdd9cc214cdc
FranklinOtoni <fsotoni@yahoo.com>
2021-10-01 17:48:40 -0300

    incluido a funçao de autenticação

--- agenda/urls.py
+++ agenda/urls.py
@@ -20,8 +20,11 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('eventos/<titulo_evento>', views.eventos),
+    #path('eventos/<titulo_evento>', views.eventos),
     path('agenda/',views.lista_eventos),
     #path('', views.index),
     path('', RedirectView.as_view(url='/agenda/')),
+    path('login/', views.login_user),
+    path('login/submit', views.submit_login),
+    path('logout/', views.logout_user),
 ]
