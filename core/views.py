ef77c89f11fabc79cdc89b619349fddbf3e15406
FranklinOtoni <fsotoni@yahoo.com>
2021-09-30 22:49:11 -0300

    Atualização de exercício, incluir campo local na tabela, criar uma url de pesquisa, executar a pesquisa e retornar o local

--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse
+from core.models import Evento
 # Create your views here.
+
+def eventos(request, titulo_evento):
+    evento = Evento.objects.get(titulo__iexact=titulo_evento)
+    local_evento = evento.local
+    return HttpResponse('Segue o local do evento: {}'.format(local_evento))
